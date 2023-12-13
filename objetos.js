5ab27b25bb4826f5e1496373c5fd8930efdb84c2
Stéphanie Fernandes <108842254+Ste-nandes@users.noreply.github.com>
2023-12-12 23:23:51 -0300

    Add files via upload

---
+++ objetos.js
@@ -0,0 +1,18 @@
+var objImagens = [
+    {
+        cabecalho: ["_logo.png",],
+        home: ["a folou home.png", "cachorro natalino.png", "dog 1 - aurora.png", "dog 2 - billie.png", "cat 1 - finneas.png"],
+        cadastro: ["cachorro-login"],
+        login: ["cachorro-login"],
+        adote: ["dog 1 - aurora.png", "dog 2 - billie.png", "cat 1 - finneas.png", "dog 3 - Mel.png", "cat 2 - Doja Cat.png", "cat 3 - Bey.png"],
+        denuncie: ["cachorro-denuncia.png"],
+    }
+];
+
+var objAnimais = [
+   {
+        especies: ["gato", "cachorro"],
+        sexo: ["femea", "macho"],
+        porte: ["grande", "medio", "pequeno"],
+    }
+];
